e2777945bf57d0d5f97d33288e7401532acb57d4
Alper <alpersarginercun@gmail.com>
2023-09-07 16:51:45 +0300

    login/register

---
+++ app/src/main/java/com/example/hastanerandevu/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.hastanerandevu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hastanerandevu.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
